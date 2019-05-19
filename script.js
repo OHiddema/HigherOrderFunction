c313ccf7c7d662aae9465a5df4d0a1edfe9b2603
Oege Hiddema <oegehiddema@gmail.com>
2019-05-19 10:46:33 +0200

    Step 2

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function multiplyByTwo(arr) {
+function multiplyByTwo(arr, instruction) {
     for (let i=0; i<arr.length; i++) {
-        arr[i] *=2;
+        arr[i] = instruction(arr[i]);
     }
     return arr;
 }
 
+
+function myFunction(item) {
+    return 2*item;
+}
+
 let myArr = [1, 2, 3, 4, 5];
 console.log(myArr);
-myArr = multiplyByTwo(myArr);
+myArr = multiplyByTwo(myArr, myFunction);
 console.log(myArr);
